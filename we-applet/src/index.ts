9fffbf5fbddb627cd9bd90345119b5ff2096c8e9
pospi <pospi@spadgos.com>
2023-01-24 19:08:15 +1000

    revise init logic for WeApplet for new @holochain module versions

--- we-applet/src/index.ts
+++ we-applet/src/index.ts
@@ -8,11 +8,11 @@ import {
   WeApplet,
   AppletRenderers,
   WeServices,
-  InstalledAppletInfo,
+  AppletInfo,
 } from "@neighbourhoods/nh-we-applet";
 import { ProviderStore } from "@neighbourhoods/provider-applet";
 import { ProviderApplet } from "./provider-applet";
-import { AppAgentClient } from '@holochain/client';
+import { AppAgentWebsocket } from '@holochain/client';
 
 const PROVIDER_ROLE_NAME = 'provider'
 
@@ -21,7 +21,7 @@ const providerApplet: WeApplet = {
     appWebsocket: AppWebsocket,
     adminWebsocket: AdminWebsocket,
     weStore: WeServices,
-    appletAppInfo: InstalledAppletInfo[]
+    appletAppInfo: AppletInfo[]
   ): Promise<AppletRenderers> {
     return {
       full(element: HTMLElement, registry: CustomElementRegistry) {
@@ -29,14 +29,26 @@ const providerApplet: WeApplet = {
         element.innerHTML = `<provider-applet></provider-applet>`;
         const appletElement = element.querySelector("provider-applet") as any;
 
-        const appId = appletAppInfo[0].installedAppInfo.installed_app_id
-        const providerStore = new ProviderStore(
-          new AppAgentClient(appWebsocket, appId),
-          PROVIDER_ROLE_NAME,
-        )
-        appletElement.providerStore = providerStore;
-        appletElement.appletAppInfo = appletAppInfo;
-        appletElement.sensemakerStore = weStore.sensemakerStore;
+        const appId = appletAppInfo[0].appInfo.installed_app_id
+        const installedCells = appletAppInfo[0].appInfo.cell_info[PROVIDER_ROLE_NAME]
+          // @ts-ignore
+          .map((c: CellInfo) => (c[CellType.Provisioned] || c[CellType.Cloned]) as InstalledCell);
+        const providerCell = installedCells
+          .find(c => c.role_name === PROVIDER_ROLE_NAME)
+
+        if (providerCell) {
+          AppAgentWebsocket.connect(
+            appWebsocket.client.socket.url,
+            PROVIDER_ROLE_NAME,
+          ).then(appWs => {
+            const providerStore = new ProviderStore(appWs, providerCell)
+            appletElement.providerStore = providerStore;
+            appletElement.appletAppInfo = appletAppInfo;
+            appletElement.sensemakerStore = weStore.sensemakerStore;
+          })
+        } else {
+          throw new Error("Unable to render WeApplet: no provider Cell detected")
+        }
       },
       blocks: [],
     };
