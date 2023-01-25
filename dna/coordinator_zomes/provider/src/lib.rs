1194e44f4bc12de24c54a9dab4d1dd385a20a1bf
Wesley Finck <wesleyfinck@gmail.com>
2023-01-25 17:16:17 -0600

    added basic zome code

---
+++ dna/coordinator_zomes/provider/src/lib.rs
@@ -0,0 +1,11 @@
+use hdk::prelude::*;
+use provider_integrity::{EntryTypes, ProviderEntry};
+
+#[hdk_extern]
+pub fn create_provider_entry(
+    provider_entry: ProviderEntry
+) -> ExternResult<ActionHash> {
+    let action_hash = create_entry(EntryTypes::ProviderEntry(provider_entry.clone()))?;
+    let _entry_hash = hash_entry(provider_entry.clone())?;
+    Ok(action_hash)
+}
